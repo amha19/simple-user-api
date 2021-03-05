0baa6d2c64a36228b17d629ea5aa4833da74f0d1
amha19 <34722096+amha19@users.noreply.github.com>
2021-03-05 14:28:31 -0600

    added post and get by user name

--- app.js
+++ app.js
@@ -21,6 +21,26 @@ app.get('/users', (req, res) => {
     });
 });
 
+app.post('/users', (req, res) => {
+    const { name, email } = req.body;
+    if (name && email) {
+        users.push({ name, email });
+    }
+    res.json({
+        msg: 'User created',
+    });
+});
+
+app.get('/users/:userName', (req, res) => {
+    const { userName } = req.params;
+
+    const user = users.find((user) => user.name === userName);
+
+    if (!user) return res.json({ msg: 'User not found' });
+
+    res.json(user);
+});
+
 app.listen(port, () => {
     console.log(`app is running on port: ${port}`);
 });
